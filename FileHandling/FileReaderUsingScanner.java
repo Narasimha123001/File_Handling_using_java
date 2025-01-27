a79ebae779b211b2152aee73e518e8b62c234edd
Narasimha <narasimhaanguluri001@gmail.com>
2025-01-27 23:58:27 +0530

    File reader using the fileReader in java

---
+++ FileHandling/FileReaderUsingScanner.java
@@ -0,0 +1,19 @@
+package com.tecktricks.FileHandling;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReaderUsingScanner {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(new File("src/com/tecktricks/FileHandling/Create.txt"))) {
+            String line;
+            while(scanner.hasNext()){
+                line= scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
