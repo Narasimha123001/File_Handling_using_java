2380ec73248dd48394110cc4887d5164514c7e55
Narasimha <narasimhaanguluri001@gmail.com>
2025-01-27 23:59:57 +0530

    Reader the file usong Scanner Class

--- FileHandling/FileReaderUsingScanner.java
+++ FileHandling/FileReaderUsingScanner.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class FileReaderUsingScanner {
     public static void main(String[] args) {
+
         try (Scanner scanner = new Scanner(new File("src/com/tecktricks/FileHandling/Create.txt"))) {
             String line;
             while(scanner.hasNext()){
